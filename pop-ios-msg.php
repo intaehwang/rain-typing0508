e615c949249efe5ed1004d38cdf38707aa566e65
intaehwang <131849572+intaehwang@users.noreply.github.com>
2023-05-04 15:51:21 +0900

    230504
    ISO사용자 메시지팝업 추가

---
+++ pop-ios-msg.php
@@ -0,0 +1,32 @@
+
+<div class="layer-popup" id="pop-ios-msg">
+	
+
+	<div class="popContainer">
+		
+		<div class="pop-inner">
+			<header class="pop-header">
+				알림
+				<span class="pop-closer">팝업닫기</span>
+			</header>
+			<div class="popCon">				
+				<p class="fs14rem tcenter">
+					아이폰(ISO)을 사용하시는 분들은<br>
+					G마켓 앱에서 쾌적한 플레이가 가능합니다.
+				</p>
+				<p class="fs20rem tcenter mt15">G마켓 앱으로 이동할까요?</p>
+				<div class="division-line mt25 mb25"></div>
+
+				<div class="btnSet mt20">
+					<a href="ranking_save.php" class="_btn/lg w-full fs23rem"><span class="btn-inner">네, 좋아요<br><small class="fs12rem"style="color:#606164;">(앱이 없을 경우, 앱스토어로 이동)</small></span></a>
+					<span class="_btn w-full popClose fs23rem"><span class="btn-inner">괜찮아요</span></span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="pop-bg"></div>
+</div>
+
+
+				
